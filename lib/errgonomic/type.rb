cc3d9df43c35b7af0d52730bf9f35d0d1990780b
kinduff <alejandro@onemorecloud.com>
2025-04-15 14:06:25 -0600

    Adds examples for yard

--- lib/errgonomic/type.rb
+++ lib/errgonomic/type.rb
@@ -7,6 +7,10 @@ class Object
   # @param type [Class] The expected type or module the receiver should be.
   # @param message [String] Optional error message to raise if type doesn't match.
   # @return [Object] The receiver if it is of the expected type.
+  # @example
+  #   "hello".type_or_raise!(String)   # => "hello"
+  #   123.type_or_raise!(String)       # => raises TypeMismatchError
+  #   123.type_or_raise!(String, "Not a string")  # => raises TypeMismatchError with custom message
   def type_or_raise!(type, message = nil)
     message ||= "Expected #{type} but got #{self.class}"
     raise Errgonomic::TypeMismatchError, message unless is_a?(type)
@@ -14,13 +18,16 @@ class Object
     self
   end
 
-  alias_method :type_or_raise, :type_or_raise!
+  alias type_or_raise type_or_raise!
 
   # Returns the receiver if it matches the expected type, otherwise returns the default value.
   #
   # @param type [Class] The expected type or module the receiver should be.
   # @param default [Object] The value to return if type doesn't match.
   # @return [Object] The receiver if it is of the expected type, otherwise the default value.
+  # @example
+  #   "hello".type_or(String, "default")  # => "hello"
+  #   123.type_or(String, "default")      # => "default"
   def type_or(type, default)
     return self if is_a?(type)
 
@@ -33,6 +40,9 @@ class Object
   # @param type [Class] The expected type or module the receiver should be.
   # @param block [Proc] The block to call if type doesn't match.
   # @return [Object] The receiver if it is of the expected type, otherwise the block result.
+  # @example
+  #   "hello".type_or_else(String) { "default" }  # => "hello"
+  #   123.type_or_else(String) { "default" }      # => "default"
   def type_or_else(type, &block)
     return self if is_a?(type)
 
@@ -44,6 +54,10 @@ class Object
   # @param type [Class] The type or module the receiver should not be.
   # @param message [String] Optional error message to raise if type matches.
   # @return [Object] The receiver if it is not of the specified type.
+  # @example
+  #   "hello".not_type_or_raise!(Integer)  # => "hello"
+  #   123.not_type_or_raise!(Integer)      # => raises TypeMismatchError
+  #   123.not_type_or_raise!(Integer, "Not an integer")  # => raises TypeMismatchError with custom message
   def not_type_or_raise!(type, message = nil)
     message ||= "Expected anything but #{type} but got #{self.class}"
     raise Errgonomic::TypeMismatchError, message if is_a?(type)
