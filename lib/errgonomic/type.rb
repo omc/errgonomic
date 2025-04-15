49c1630c25670374243706e1fc36c3e22aedc80d
kinduff <alejandro@onemorecloud.com>
2025-04-15 14:18:30 -0600

    Fixes yard doctest

--- lib/errgonomic/type.rb
+++ lib/errgonomic/type.rb
@@ -8,9 +8,9 @@ class Object
   # @param message [String] Optional error message to raise if type doesn't match.
   # @return [Object] The receiver if it is of the expected type.
   # @example
-  #   "hello".type_or_raise!(String)   # => "hello"
-  #   123.type_or_raise!(String)       # => raises TypeMismatchError
-  #   123.type_or_raise!(String, "Not a string")  # => raises TypeMismatchError with custom message
+  #   'hello'.type_or_raise!(String) #=> "hello"
+  #   123.type_or_raise!(String, "We need a string!") #=> raise Errgonomic::TypeMismatchError, "We need a string!"
+  #   123.type_or_raise!(String) #=> raise Errgonomic::TypeMismatchError, "Expected String but got Integer"
   def type_or_raise!(type, message = nil)
     message ||= "Expected #{type} but got #{self.class}"
     raise Errgonomic::TypeMismatchError, message unless is_a?(type)
@@ -26,8 +26,8 @@ class Object
   # @param default [Object] The value to return if type doesn't match.
   # @return [Object] The receiver if it is of the expected type, otherwise the default value.
   # @example
-  #   "hello".type_or(String, "default")  # => "hello"
-  #   123.type_or(String, "default")      # => "default"
+  #   'hello'.type_or(String, 'default')  # => "hello"
+  #   123.type_or(String, 'default')      # => "default"
   def type_or(type, default)
     return self if is_a?(type)
 
@@ -41,8 +41,8 @@ class Object
   # @param block [Proc] The block to call if type doesn't match.
   # @return [Object] The receiver if it is of the expected type, otherwise the block result.
   # @example
-  #   "hello".type_or_else(String) { "default" }  # => "hello"
-  #   123.type_or_else(String) { "default" }      # => "default"
+  #   'hello'.type_or_else(String) { 'default' }  # => "hello"
+  #   123.type_or_else(String) { 'default' }      # => "default"
   def type_or_else(type, &block)
     return self if is_a?(type)
 
@@ -55,9 +55,9 @@ class Object
   # @param message [String] Optional error message to raise if type matches.
   # @return [Object] The receiver if it is not of the specified type.
   # @example
-  #   "hello".not_type_or_raise!(Integer)  # => "hello"
-  #   123.not_type_or_raise!(Integer)      # => raises TypeMismatchError
-  #   123.not_type_or_raise!(Integer, "Not an integer")  # => raises TypeMismatchError with custom message
+  #   'hello'.not_type_or_raise!(Integer) #=> "hello"
+  #   123.not_type_or_raise!(Integer, "We dont want an integer!") #=> raise Errgonomic::TypeMismatchError, "We dont want an integer!"
+  #   123.not_type_or_raise!(Integer) #=> raise Errgonomic::TypeMismatchError, "Expected anything but Integer but got Integer"
   def not_type_or_raise!(type, message = nil)
     message ||= "Expected anything but #{type} but got #{self.class}"
     raise Errgonomic::TypeMismatchError, message if is_a?(type)
