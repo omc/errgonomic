4a9430a0edd1b68b9397c5beffa86fe8011e6bdd
Nick Zadrozny <nick@onemorecloud.com>
2025-07-09 10:07:56 -0600

    wip

--- lib/errgonomic/rails.rb
+++ lib/errgonomic/rails.rb
@@ -3,6 +3,8 @@ require_relative 'rails/active_record_optional'
 require_relative 'rails/active_record_delegate_optional'
 
 module Errgonomic
+  # Slightly more convenient access to the setup functions:
+  # Errgonomic::Rails.setup_before and Errgonomic::Rails.setup_after
   module Rails
     # We provide helper class methods, like `delegate_optional`,
     # which need to be included into ActiveRecord::Base before any models are
@@ -17,8 +19,12 @@ module Errgonomic
     def self.setup_after
       Zeitwerk::Loader.eager_load_all
       ActiveRecord::Base.descendants.each do |model|
-        next unless model.table_exists? rescue false
-        puts "errgonomic making #{model.name} optional"
+        next unless begin
+          model.table_exists?
+        rescue StandardError
+          false
+        end
+
         model.include Errgonomic::Rails::ActiveRecordOptional
       end
     end
@@ -27,7 +33,6 @@ module Errgonomic
   # Hook into Rails with a Railtie
   class Railtie < ::Rails::Railtie
     initializer 'errgonomic.setup_before' do
-      puts "errgonomic railtie initializer to_prepare setup_before"
       Errgonomic::Rails.setup_before
     end
     config.after_initialize do
@@ -36,7 +41,6 @@ module Errgonomic
       end
     end
     config.to_prepare do
-      puts "errgonomic railtie config.to_prepare setup_after"
       Errgonomic::Rails.setup_after
     end
   end
