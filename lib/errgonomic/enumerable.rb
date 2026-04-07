6c66983bd1db08f09332d4e96e600523a704aa67
Nick Zadrozny <nick@onemorecloud.com>
2026-04-07 18:16:30 -0500

    generic sequence is not useful, moving back to a separate collect_options and collect_results

--- lib/errgonomic/enumerable.rb
+++ lib/errgonomic/enumerable.rb
@@ -1,47 +1,66 @@
 module Enumerable
-  # Collect an Enumerable of Results into a Result of an Array. Or collects the
-  # Enumerable of Options into an Option of an Array. Short-circuits on the
-  # first Err or None.
+  # Collect an Enumerable of Results into a Result of an Array. Short-circuits
+  # on the first Err.
   #
   # @example
-  #   [].sequence                      # => Ok([])
-  #   [Ok(1), Ok(2), Ok(3)].sequence   # => Ok([1, 2, 3])
-  #   [Ok(1), Err(:x), Ok(3)].sequence # => Err(:x)
-  #   [Ok(1), "oops", Ok(3)].sequence  # => raise Errgonomic::ArgumentError, "sequence expects every element to be a Result, got String"
-  #   [Ok(1), Some(2), Ok(3)].sequence # => raise Errgonomic::ArgumentError, "sequence expects every element to be the same kind of Result or Option"
-  #   ["shrug"].sequence               # => raise Errgonomic::ArgumentError, "sequence must be called on an enumerable of Results or Options"
+  #   [].collect_results                      # => Ok([])
+  #   [Ok(1), Ok(2), Ok(3)].collect_results   # => Ok([1, 2, 3])
+  #   [Ok(1), Err(:x), Ok(3)].collect_results # => Err(:x)
+  #   [Ok(1), "oops", Ok(3)].collect_results  # => raise Errgonomic::ArgumentError, "collect_results expects every element to be a Result, got String"
+  #   [Ok(1), Some(2), Ok(3)].collect_results # => raise Errgonomic::ArgumentError, "collect_results expects every element to be a Result, got Errgonomic::Option::Some"
   #
   # @return [Errgonomic::Result::Any]
-  def sequence
+  def collect_results
     return Ok([]) if empty?
 
     values = []
 
-    sequence_inner_type = find { |e| e.is_a?(Errgonomic::Result::Any) || e.is_a?(Errgonomic::Option::Any) }
-    if sequence_inner_type.nil?
-      raise Errgonomic::ArgumentError, "sequence must be called on an enumerable of Results or Options"
-    end
-    sequence_inner_type = sequence_inner_type.class.superclass
     each do |element|
-      # defensive runtime checks about the contents of the enumerable
       if Errgonomic.give_me_ambiguous_downstream_errors?
-        # must be
-        if !element.is_a?(sequence_inner_type) && (element.is_a?(Errgonomic::Result::Any) || element.is_a?(Errgonomic::Option::Any))
-          raise Errgonomic::ArgumentError, "sequence expects every element to be the same kind of Result or Option"
-        end
-
-        if !element.is_a?(sequence_inner_type)
-          result_or_option = sequence_inner_type == Errgonomic::Result::Any ? "Result" : "Option"
-          raise Errgonomic::ArgumentError, "sequence expects every element to be a #{result_or_option}, got #{element.class}"
+        # defensive runtime checks about the contents of the enumerable
+        if !element.is_a?(Errgonomic::Result::Any)
+          raise Errgonomic::ArgumentError, "collect_results expects every element to be a Result, got #{element.class}"
         end
       end
 
-      return element if element.is_a?(Errgonomic::Result::Any) && element.err?
-      return element if element.is_a?(Errgonomic::Option::Any) && element.none?
+      return element if element.err?
 
       values << element.value
     end
+
     Ok(values)
+  end
+
+  # Collect an Enumerable of Options into an Option of an Array. Short-circuits
+  # on the first None.
+  #
+  # @example
+  #   [].collect_options                          # => Some([])
+  #   [Some(1), Some(2), Some(3)].collect_options # => Some([1, 2, 3])
+  #   [Some(1), None(), Some(3)].collect_options  # => None()
+  #   [Some(1), "oops", Some(3)].collect_options  # => raise Errgonomic::ArgumentError, "collect_options expects every element to be a Option, got String"
+  #   [Some(1), Ok(2), Some(3)].collect_options   # => raise Errgonomic::ArgumentError, "collect_options expects every element to be a Option, got Errgonomic::Result::Ok"
+  #
+  # @return [Errgonomic::Result::Any]
+  def collect_options
+    return Some([]) if empty?
+
+    values = []
+
+    each do |element|
+      if Errgonomic.give_me_ambiguous_downstream_errors?
+        # defensive runtime checks about the contents of the enumerable
+        if !element.is_a?(Errgonomic::Option::Any)
+          raise Errgonomic::ArgumentError, "collect_options expects every element to be a Option, got #{element.class}"
+        end
+      end
+
+      return element if element.none?
+
+      values << element.value
+    end
+
+    Some(values)
   end
 
 
